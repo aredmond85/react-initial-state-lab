4e51ea0772c4861da43a2dc1bfbe1a5514fe1f2c
Austin <aredmond85@gmail.com>
2021-03-06 21:23:49 -0600

    commit tree 4fb47cf9e746f2b27fb8c06aa2943e5b29b83413

--- src/Bomb.js
+++ src/Bomb.js
@@ -3,21 +3,23 @@ import React from 'react';
 
 class Bomb extends React.Component {
     constructor(props) {
-      super(props);
-      // Only change code below this line
-      this.state = {
-        secondsLeft: props.initialCount
-      }
-      // Only change code above this line
+        super(props);
+        // Only change code below this line
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+        // Only change code above this line
     }
     render() {
-        const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
-      return (
-        <div>
-          {message}
-        </div>
-      );
+        const message = this.state.secondsLeft === 0
+            ? 'Boom!'
+            : `${this.state.secondsLeft} seconds left before I go boom!`;
+        return (
+            <div>
+                {message}
+            </div>
+        );
     }
-  };
+};
 
-  export default Bomb;
+export default Bomb;
